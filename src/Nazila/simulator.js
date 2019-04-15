bc235d77c0a0abbd59963db4a1c204607b7fcddd
Nazila <nazila@kingds.network>
2019-04-15 15:01:11 -0400

    update

--- src/Nazila/simulator.js
+++ src/Nazila/simulator.js
@@ -3,9 +3,9 @@ let mathLib = require('./mathLib.js')
 let snippet = require('./modelSnippet.js')
 
 simulator = {}
-simulator.simulate = function (particles, k, tdata, deltaT, dt, timeCountData, interpolPop, interpolBirth,params, t1,t2 ) {
+simulator.simulate = function (p, k, tdata, deltaT, dt, timeCountData, interpolPop, interpolBirth,params, t1,t2 ) {
   var st, S, E, I, R, H 
-  S = particles[0]; E = particles[1]; I = particles[2]; R = particles[3]; H = particles[4]
+  S = p[0]; E = p[1]; I = p[2]; R = p[3]; H = p[4]
   // transitions between classes
   if (k <= tdata || k > t1) {
     steps = mathLib.numMapSteps(k, k + deltaT, dt)
@@ -16,7 +16,7 @@ simulator.simulate = function (particles, k, tdata, deltaT, dt, timeCountData, i
   for (let stp = 0; stp < steps; stp++) { // steps in each time interval
     st = k + stp * del_t
     simulateValue = snippet.rprocess(params, st, del_t, [S,E,I,R,H], interpolPop(st), interpolBirth(st))
-    S = simulateValue[0]; E = simulateValue[1], I = simulateValue[2], R = simulateValue[3], H = simulateValue[4]
+    S = simulateValue[0]; E = simulateValue[1]; I = simulateValue[2]; R = simulateValue[3]; H = simulateValue[4]
   }
   return [S, E, I, R, H]
 }
