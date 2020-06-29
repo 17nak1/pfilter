03598053c18608faace070c08691bf7491dbcb6e
Amir <amirkharazian@gmail.com>
2020-06-29 19:13:30 +0430

    np change

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ myPomp = new pomp({
 });
 
 current_params= {R0: 3.132490e+01 , amplitude: 3.883620e-01 , gamma: 7.305000e+01 , mu: 6.469830e-04 , sigma: 4.566000e+01 ,rho:  4.598709e-01 ,psi:  1.462546e-01 ,S_0:  3.399189e-02 ,E_0: 2.336327e-04 ,R_0: 9.657741e-01,I_0: 4.221789e-07}
-np= 20000;
+np= 200;
 
 ss = myPomp.pfilter({
   params: current_params,
